70befa31d19630a800bc09839e8b37db21ca0fc9
Emad Alashi <emad.ashi@gmail.com>
2014-05-01 18:41:37 +1000

    init commit

---
+++ AzureMeetup2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AzureMeetup2.MvcApplication" Language="C#" %>
---
+++ AzureMeetup2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AzureMeetup2.Startup))]
+namespace AzureMeetup2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
